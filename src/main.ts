a1c3baa5c09fd1bdc63f569a10c0b0ac1cb06ee2
yahiousun <yahiousun@gmail.com>
2017-04-04 13:30:46 +0800

    Add Material dependency

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { ContentRequestMessageService, MethodType } from './packages/request-message-service';
+// import { ContentRequestMessageService, MethodType } from './packages/request-message-service';
 
 if (environment.production) {
   enableProdMode();
@@ -11,14 +11,14 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
-const messageService = new ContentRequestMessageService();
+// const messageService = new ContentRequestMessageService();
 
-const params = window.location.search.split('?')[1];
+// const params = window.location.search.split('?')[1];
 
-const sessionId = params.split('=')[1];
+// const sessionId = params.split('=')[1];
 
-messageService.respond({
-  jsonrpc: '2.0',
-  id: sessionId,
-  result: 'Application Start'
-});
+// messageService.respond({
+//   jsonrpc: '2.0',
+//   id: sessionId,
+//   result: 'Application Start'
+// });
